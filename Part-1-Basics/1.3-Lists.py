fcee910073b1bfd30197c999c14f9859bc65a54b
Ashish <s8asranj@stud.uni-saarland.de>
2026-09-22 21:46:51 +0200

    some more work on my first script

---
+++ Part-1-Basics/1.3-Lists.py
@@ -0,0 +1,4 @@
+list = ["ashish", "ranjan", 4]
+
+for i in list:
+    print((i*2))
--- Personal-Scripts/StudyLogger.py
+++ Personal-Scripts/StudyLogger.py
@@ -16,13 +16,45 @@ whatIStudied = input()
 print(f"Great that you studied {whatIStudied}!")
 
 print("And how many hours?")
-time = input()
+time = int(input())
 print(f"So we studied {whatIStudied} for {time} hours.")
 
 date = datetime.datetime.now()
 dateToday= date.strftime("%x")
 print("Today's date is: " + dateToday)
 
+
+#################################
+def stringChecker():
+    flag=True 
+    if whatIStudied.isdigit():
+        flag = False
+    elif whatIStudied == "":
+        flag = False
+    elif whatIStudied[0].isdigit():
+         flag = False
+    elif whatIStudied[0] == '-':
+         flag = False
+    return flag
+#################################
+
+## File opening, writing and closing 
 file = open("studyLogger.txt")
+
+#write the file
 with open ("studyLogger.txt", "a") as file:
-    file.write(f"{whatIStudied}--{time}--{dateToday}")
+        isEnteredStringValid = stringChecker() 
+        if isEnteredStringValid==True:      
+            file.write(f"{whatIStudied}--{time}--{dateToday}\n")
+            openFile = True
+        else:   
+            print("Please enter a valid string for what you studied.")
+            openFile = False
+
+#close before reading again
+file.close()
+
+#read the whole file again
+if openFile == True:
+    file = open("studyLogger.txt")
+    print(file.read())
